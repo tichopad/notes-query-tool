1d89e87395547ab9181fb5f64cd1218cab828f68
Michael Tichopad <michael@tichopad.dev>
2026-04-14 21:25:53 +0200

    Fiat lux

---
+++ src/load-files.ts
@@ -0,0 +1,7 @@
+import { Glob } from "bun"
+
+export function loadFiles(globPattern: string): AsyncIterable<string> {
+  const glob = new Glob(globPattern);
+
+  return glob.scan({ onlyFiles: true, dot: false, absolute: true });
+}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { defineCommand, runMain } from "citty";
+import { loadFiles } from "./load-files";
+
+const main = defineCommand({
+  meta: {
+    name: "notes-query-tool",
+    version: "1.0.0",
+    description: "My Awesome CLI App",
+  },
+  args: {
+    pattern: {
+      type: "positional",
+      description: "Files glob",
+      required: true,
+    },
+  },
+  setup({ args }) {
+    console.log(`now setup ${args.pattern}`);
+  },
+  cleanup({ args }) {
+    console.log(`now cleanup ${args.pattern}`);
+  },
+  async run({ args }) {
+    for await (const file of loadFiles(args.pattern)) {
+      console.log(file);
+    }
+  },
+});
+
+runMain(main);
